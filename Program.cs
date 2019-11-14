31fb78390ede719aff1d11ca1140eaeaa9e4885e
9kongpob <k.kunchanuan@hotmail.com>
2019-11-14 17:41:03 +0700

    Push 01

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace BasicPart07_Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number;
+            //showMessage("Kongpop Kunchanuan");
+
+            Console.Write("Input number : ");
+            int.TryParse(Console.ReadLine(), out number);
+            while (number != 0)//ค่าที่ไม่เท่ากับ 0 ถึงจะเข้าสู่ loop while
+            {
+
+                showMultipleTable(number);
+
+                Console.Write("Input number : ");
+                int.TryParse(Console.ReadLine(), out number);
+                //เอาค่าที่รับมาใหม่ไปเช็ค
+            } 
+            
+            //Console.ReadKey();
         }
+
+/*        static void showMessage(string str)
+        {
+            Console.WriteLine("Hello K." + str);
+        }*/
+
+        static void showMultipleTable(int number)
+        {
+            for (int i=1; i <= 12;i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}",number,i,number*i);
+            }
+        }
+
+
     }
 }
